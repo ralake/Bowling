a4090753bb29ca9225ab05d82ebb11e89dbe2365
Richard Lake <ralake1985@gmail.com>
2014-12-13 20:08:33 +0000

    Add spare bonus points functioanlity

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -1,11 +1,13 @@
 describe('Bowling Game', function() {
 
 	var game;
-	var frame;
+	var frame1;
+	var frame2;
 
 	beforeEach(function() {
 		game = new Game();
-		frame = new Frame();
+		frame1 = new Frame();
+		frame2 = new Frame();
 	});
 
 	describe('Frames...', function() {
@@ -15,13 +17,56 @@ describe('Bowling Game', function() {
 		});
 
 		it('can have a frame added to it', function() {
-			game.addFrame(frame);
+			game.addFrame(frame1);
 			expect(game.frames.length).toBe(1);
+		});
+
+		it('can have no more than ten frames', function() {
+			for(var i = 0; i < 10; i++) {
+				game.addFrame(new Frame);
+			};
+			expect(game.frames.length).toBe(10)
+			expect( function(){ game.addFrame(frame1); } ).toThrow(new Error("There are already ten frames"));
 		});
 
 	});
 
-	describe('Scores...', function() {
+	describe('Basic Scores...', function() {
+
+		it('has a score of zero at the start of the game', function() {
+			expect(game.score).toBe(0)
+		});
+
+		it('adds the score of the frame to the total when the frame is added', function() {
+			frame1.roll1(2);
+			frame1.roll2(4);
+			game.addFrame(frame1)
+			expect(game.score).toBe(6);
+		});
+
+		it('keeps a running total', function() {
+			frame1.roll1(2);
+			frame1.roll2(4);
+			frame2.roll1(5);
+			frame2.roll2(3);
+			game.addFrame(frame1)
+			game.addFrame(frame2)
+			expect(game.score).toBe(14);
+		});
+
+	});
+
+	describe('Bonus Scores...', function() {
+
+		it('will add the score of the first roll of the next frame if a player score a spare', function() {
+			frame1.roll1(6);
+			frame1.roll2(4);
+			game.addFrame(frame1)
+			frame2.roll1(1);
+			frame2.roll2(1);
+			game.addFrame(frame2)
+			expect(game.score).toBe(13);
+		});
 
 	});
 
--- src/game.js
+++ src/game.js
@@ -1,7 +1,40 @@
 function Game() {
-	this.frames = []
+	this.frames = [];
+	this.score = 0;
 };
 
 Game.prototype.addFrame = function(frame) {
+	if (this.frames.length === 10) {
+		throw Error("There are already ten frames");
+	}
+	else {
 	this.frames.push(frame);
+	this.calculateBonus(frame);
+	this.calculateTotalScore();
+	}
+};
+
+Game.prototype.spareBonus = function(frame) {
+	var previousFrame = this.frames[this.frames.indexOf(frame) - 1];
+	if (previousFrame.isSpare()) {
+		previousFrame.bonus += frame.roll1Score;
+	}
+	else {
+	}
+};
+
+Game.prototype.calculateBonus = function(frame) {
+	if (this.frames.length > 1) {
+		this.spareBonus(frame);
+	}
+	else {
+	}
+};
+
+Game.prototype.calculateTotalScore = function() {
+	var score = 0
+	this.frames.forEach(function(frame) {
+		score += (frame.pinsHit + frame.bonus)
+	});
+	this.score = score;
 };
