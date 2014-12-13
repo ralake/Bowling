dc4f78ddf8295915906047443529459c8dd4bcce
Richard Lake <ralake1985@gmail.com>
2014-12-13 18:00:33 +0000

    Add illegalRoll function

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -3,41 +3,58 @@ describe('Frames', function() {
 	var frame;
 
 	beforeEach(function() {
-		frame = new Frame;
+		frame = new Frame();
 	});
 
-	it('should record the score of a roll', function() {
-		frame.roll1(5);
-		expect(frame.roll1Score).toBe(5);
+	describe('scores...', function() {
+
+		it('should record the score of a roll', function() {
+			frame.roll1(5);
+			expect(frame.roll1Score).toBe(5);
+		});
+
+		it('should know the score of both rolls', function() {
+			frame.roll1(5);
+			frame.roll2(3);
+			expect(frame.pinsHit).toBe(8);
+		});
+
+		it('should not be able to record more than 10 pins being knocked down in a single roll', function() {
+			expect( function(){ frame.roll1(12); } ).toThrow(new Error("There are only ten pins"));
+		});
+
+		it('should never have more than ten pins hit per frame', function() {
+			frame.roll1(6);
+			expect( function(){ frame.roll2(8); } ).toThrow(new Error("There are only ten pins"));
+		});
+
 	});
 
-	it('should know the score of both rolls', function() {
-		frame.roll1(5);
-		frame.roll2(3);
-		expect(frame.pinsHit).toBe(8);
-	});
+	describe('strikes and spares...', function() {
 
-	it('should know if it is a spare', function() {
-		frame.roll1(5);
-		frame.roll2(5);
-		expect(frame.isSpare()).toBe(true);
-	});
+		it('should know if it is a spare', function() {
+			frame.roll1(5);
+			frame.roll2(5);
+			expect(frame.isSpare()).toBe(true);
+		});
 
-	it('should know if it is not a spare', function() {
-		frame.roll1(5);
-		frame.roll2(4);
-		expect(frame.isSpare()).toBe(false);
-	});
+		it('should know if it is not a spare', function() {
+			frame.roll1(5);
+			frame.roll2(4);
+			expect(frame.isSpare()).toBe(false);
+		});
 
-	it('should know if it is a strike', function() {
-		frame.roll1(10);
-		expect(frame.isStrike()).toBe(true);
-	});
+		it('should know if it is a strike', function() {
+			frame.roll1(10);
+			expect(frame.isStrike()).toBe(true);
+		});
 
-	it('should know if it is not a strike', function() {
-		frame.roll1(2);
-		frame.roll2(2);
-		expect(frame.isStrike()).toBe(false);
+		it('should know if it is not a strike', function() {
+			frame.roll1(2);
+			frame.roll2(2);
+			expect(frame.isStrike()).toBe(false);
+		});
+
 	});
 
 });
--- src/frame.js
+++ src/frame.js
@@ -3,11 +3,13 @@ function Frame() {
 };
 
 Frame.prototype.roll1 = function(pinsHit) {
+	this._illegalRoll(pinsHit);
 	this.roll1Score = pinsHit;
 	this.pinsHit += pinsHit;
 };
 
 Frame.prototype.roll2 = function(pinsHit) {
+	this._illegalRoll(pinsHit);
 	this.roll2Score = pinsHit;
 	this.pinsHit += pinsHit;
 };
@@ -16,5 +18,13 @@ Frame.prototype.isSpare = function() {
 	return this.roll1Score < 10 && this.pinsHit === 10;
 };
 Frame.prototype.isStrike = function() {
-	return (this.roll1Score || this.roll2Score) === 10
+	return (this.roll1Score || this.roll2Score) === 10;
+};
+
+Frame.prototype._illegalRoll = function(pinsHit) {
+	if (pinsHit > 10 || (this.pinsHit + pinsHit > 10)) {
+		throw Error("There are only ten pins");
+	}
+	else {
+	}
 };
